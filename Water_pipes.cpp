19dbe28976514cd802b78ee9cfd7fb56672c5127
Tomasz Nazar <tomeknazz@gmail.com>
2024-02-28 19:01:45 +0100

    test

--- Water_pipes.cpp
+++ Water_pipes.cpp
@@ -1,12 +1,14 @@
-// Water_pipes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    cout << "Hello World!" << endl;
 }
+
 
 
 
